2721e148255f8157218108f89d1898ec0aca0e32
satvik-tha-god <satviksinghx@gmail.com>
2021-08-29 04:26:04 +0530

    added keeper

---
+++ Keeper/src/notes.js
@@ -0,0 +1,24 @@
+const notes =[
+  {
+    key:1,
+    title: "DSA",
+    content: "Solve Cracking The Coding Interview and IET Brute Force Made Easy"
+  },
+  {
+    key:2,
+    title: "Development",
+    content: "Make a MERN stack project, make a Machine Learning project, make a web scraping project"
+  },
+  {
+    key:3,
+    title: "Theory",
+    content: "Learn C programming basics, Operating Systems, DBMS, Object Oriented Programming and Computer Networking"
+  },
+  {
+    key:4,
+    title: "Experience",
+    content: "Do the LGM VIP tasks and Cob:web-Aclevo CelOS and Whittr "
+  }
+];
+
+export default notes;
